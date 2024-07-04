2793ab01db6063e75618dc7e108860ea520961ea
sai-965 <sai96528@gmail.com>
2024-07-04 19:55:33 +0530

    Validate a numeric single digit

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,11 +2,16 @@ import Add from './index';
 
 describe('String calculator', () => {
   test('Should return 0 when empty value is passed as input', () => {
-    expect(Add('')).toBe(0)
-  })
+    expect(Add('')).toBe(0);
+  });
 
   test('Should return 0 when only alphabet is passed as input', () => {
-    expect(Add('a')).toBe(0)
-    expect(Add('abc')).toBe(0)
-  })
+    expect(Add('a')).toBe(0);
+    expect(Add('abc')).toBe(0);
+  });
+
+  test('Should return the same number when only character is passed as input', () => {
+    expect(Add('1')).toBe(1);
+    expect(Add('4')).toBe(4);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ function Add(numbers: string): number | string {
   let sum = 0;
   if (numbers.length) {
     if (numbers.length === 1) {
+      sum = Number(numbers);
     } else if (numbers.length > 1) {
     }
     const isNotANumber = isNaN(Number(numbers));
